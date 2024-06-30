60344bcfafb83880575c301c9c24e83df7fa3d1c
jatinbaroliya <jatinbaroliya31@gmail.com>
2024-06-30 15:19:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState , useEffect} from 'react'
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const [showFinished, setshowFinished] = useState(true)
+
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos")
+    if(todoString){
+      let todos = JSON.parse(localStorage.getItem("todos")) 
+      setTodos(todos)
+    }
+  }, [])
+
+  const saveToLS = (params) => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+
+  const handleDelete= (e, id)=>{  
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    }); 
+    setTodos(newTodos) 
+    saveToLS()
+  }
+  const handleEdit = (e, id)=>{ 
+    let t = todos.filter(i=>i.id === id) 
+    setTodo(t[0].todo)
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    }); 
+    setTodos(newTodos) 
+    saveToLS()
+  }
+  const handleAdd = (e) => {
+    setTodos([...todos, {id:uuidv4(), todo, isCompleted: false}])
+    setTodo("") 
+    saveToLS()
+  }
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+  const handleCheckbox = (id) => {
+    const newTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodos(newTodos);
+    saveToLS()
+  }
+  const toggleFinished = (e) => {
+    setshowFinished(!showFinished)
+  }
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleAdd();
+    }
+  };
+
+  return (
+    <>
+    <div className="container bg-slate-300 lg:w-[35%] md:w-[60%] min-h-[93vh] mx-auto sm:my-5 p-5 rounded-xl">
+      <div className="font-bold text-[20px] text-center">My-Task - Manage your todos at one Place</div>
+      <div className='font-bold text-[17px] m-3'>Add a Todo</div>
+      <div className='flex justify-between'>
+      <input onChange={handleChange} onKeyDown={handleKeyDown} value={todo} type="text" className="rounded-full w-[80%] py-1 px-5 outline-none "/>
+      <button onClick={handleAdd} disabled={todo.length<1} className='rounded-full bg-purple-600 w-14 disabled:bg-purple-500 cursor-pointer' >Save</button>
+      </div>
+      <input className='mr-2 my-6 cursor-pointer' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished} /> 
+      <label className='my-6'>Show Fininshed </label>
+      <div className='h-[1px] w-[85%] mx-auto bg-gray-400 my-2'></div>
+      <div className="todos mt-6">
+        <div className='text-[16px] font-bold my-4'>Your Todos :</div>
+        {todos.length ===0 && <div className='m-5'>No Todos to display</div> }
+        {todos.map(item => {
+          return (showFinished || !item.isCompleted) && <div key={item.id} className='flex'>
+              <input className='mx-4 cursor-pointer' name={item.id}  onChange={() => handleCheckbox(item.id)} type="checkbox"  checked={item.isCompleted} id=''/>
+              <div className={`w-[250px] text-purple-900 ${item.isCompleted ? "line-through" : ""}`}>
+                {item.todo}
+              </div>
+              <button onClick={(e)=>{handleEdit(e, item.id)}} className='mx-2 text-[20px]'><FaEdit /></button>
+              <button onClick={(e)=>{handleDelete(e, item.id)}} className='mx-2 text-[21px]'><MdDelete /></button>
+            </div>
+        })}
+      </div>
+    </div>   
+    <div className='text-gray-600 text-[13px] text-center my-6'>Copyright © Made by Jatin Baroliya. All rights reserved.</div>  
+    </>
+  )
+}
+
+export default App
